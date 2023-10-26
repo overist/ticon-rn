6457bb815bd92132de144f5419a23a644f66c71b
overist <wj@over.ist>
2023-10-26 19:55:25 +0900

    feat: make splash screen

--- App.tsx
+++ App.tsx
@@ -2,11 +2,19 @@ import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./src/store/store";
 import Toast from "react-native-toast-message";
-import React from "react";
+import React, { useEffect } from "react";
 import { getAuthInstance } from "./src/modules/firebaseConfig";
 import Navigation from "./src/navigation";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
+  useEffect(() => {
+    async function hideSplashScreen() {
+      await SplashScreen.hideAsync();
+    }
+    hideSplashScreen();
+  }, []);
+
   getAuthInstance();
   return (
     <>
